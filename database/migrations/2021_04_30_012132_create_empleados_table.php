c487d104878f035199be8201e15f44fb62c82152
diego <diegosistem09@gmail.com>
2021-04-29 20:27:00 -0500

    version 1 creacion migraciones

---
+++ database/migrations/2021_04_30_012132_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+             $table->string('nombres',30);
+            $table->string('apellidos',30);
+            $table->string('identidad',20);
+            $table->text('direccion');
+            $table->string('telefono',20);
+            $table->text('ciudad_nacimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
---
+++ database/migrations/2021_04_30_012146_create_cargos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCargosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cargos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_cargo',100);
+            $table->string('ubicacion',100);
+            $table->integer('id_empleado');           
+            $table->foreign('id_empleado')->references('id')->on('empleados')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cargos');
+    }
+}
